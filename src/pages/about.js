e0eb1e745dde0707aebf8266897922ef979defa7
shicks1080 <stuart.hicks@gmail.com>
2021-08-27 14:35:50 +0200

    Styling the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react"
 
-import Layout from '../components/Layout'
+import Layout from "../components/Layout"
 
 const About = () => (
-    <Layout title="About Me">
-        <p>I work as a scientist at an up-and-coming research centre in Southern Sweden. So my days are kept busy doing my part of the installation work there! My evenings are spent trying to satisfy the healthy appetites of my 2 young boys and my husband. The boys already seem to eat more than me! They have also nudged me to experiment more with food, so it will satisfy both the adults and the children.</p>
-        <p>For my own part, I’m generally a rather active person, so food for me needs to both be satisfying and give me the energy for all my activities. Over the years I have realised that this is best achieved by maxing out the vegetable part, eating plenty of fish and minimising the meat part. In general, I’m not a fan of completely excluding anything, but rather trying to find the right balance. I find this more sustainable in the long run.</p>
-        <p>Here I will blog about my recipes and my ideas for how to approach food and time for cooking when also dealing with a busy schedule</p>
-    </Layout>
+  <Layout title="About Me">
+    <div className="bulkText">
+      <p>
+        I work as a scientist at an up-and-coming research centre in Southern
+        Sweden. So my days are kept busy doing my part of the installation work
+        there! My evenings are spent trying to satisfy the healthy appetites of
+        my 2 young boys and my husband. The boys already seem to eat more than
+        me! They have also nudged me to experiment more with food, so it will
+        satisfy both the adults and the children.
+      </p>
+      <p>
+        For my own part, I’m generally a rather active person, so food for me
+        needs to both be satisfying and give me the energy for all my
+        activities. Over the years I have realised that this is best achieved by
+        maxing out the vegetable part, eating plenty of fish and minimising the
+        meat part. In general, I’m not a fan of completely excluding anything,
+        but rather trying to find the right balance. I find this more
+        sustainable in the long run.
+      </p>
+      <p>
+        Here I will blog about my recipes and my ideas for how to approach food
+        and time for cooking when also dealing with a busy schedule
+      </p>
+    </div>
+  </Layout>
 )
 
 export default About
