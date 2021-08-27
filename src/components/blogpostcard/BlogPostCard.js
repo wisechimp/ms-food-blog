3b680c62c1191835fd523ead1e74522c710d648d
shicks1080 <stuart.hicks@gmail.com>
2021-08-27 14:02:32 +0200

    Refactoring the blogpost card and adding styling

---
+++ src/components/blogpostcard/BlogPostCard.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import LinkButton from "../linkbutton/LinkButton"
+import * as cardStyles from "./blogpostcard.module.css"
+
+const BlogPostCard = ({ title, description, date, author, slug, imageSrc }) => {
+  return (
+    <div className={cardStyles.card}>
+      <div className={cardStyles.cardTitle}>
+        <h3>{title}</h3>
+      </div>
+      <div className={cardStyles.cardBody}>
+        <div className={cardStyles.cardText}>
+          <p>
+            Written by {author} on {date}
+          </p>
+          <p>{description}</p>
+        </div>
+        <div className={cardStyles.cardThumb}>
+          <GatsbyImage image={imageSrc} alt="" />
+        </div>
+      </div>
+      <div className={cardStyles.cardButton}>
+        <LinkButton target={slug} text="Read More..." />
+      </div>
+    </div>
+  )
+}
+
+export default BlogPostCard
