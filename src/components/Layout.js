ee94877715b2229c003f067e2f2c02e4c40fa0e7
shicks1080 <stuart.hicks@gmail.com>
2021-09-03 17:20:57 +0200

    Adding the Summary Content Banner component to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,15 @@ import React from "react"
 
 import Header from "./Header"
 import Menu from "./menu/Menu"
+import SummaryContentBanner from "./summarycontentbanner/SummaryContentBanner"
 import menulinks from "../data/menulinks"
 
-const Layout = ({ title, children }) => (
+const Layout = ({ title, children, tagData }) => (
   <div>
     <Header title={title} />
     <Menu menuLinks={menulinks} />
     <h1>{title}</h1>
+    <SummaryContentBanner props={tagData} />
     {children}
   </div>
 )
