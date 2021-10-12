6b3efaf647fb3d5c08c288a0f6a643063e6ff9a0
Stuart Hicks <stuart.hicks@gmail.com>
2021-10-12 11:40:16 +0200

    Adding Contact form - thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import * as thanksStyles from "./thanks.module.css"
+
+const Thanks = () => (
+  <Layout pageTitle="Thanks!">
+    <div className={thanksStyles.thanksContent}>
+      <h3>Thank you!</h3>
+      <br />
+      <p>Thank you for taking the time to contact Ma Sharp!</p>
+      <br />
+      <p>If required I will try to get back to you as soon as possible.</p>
+    </div>
+  </Layout>
+)
+
+export default Thanks
