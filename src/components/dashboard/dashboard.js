30e5716dd417efecfcc0968cebe5bd5b34b29a35
Stuart Hicks <stuart.hicks@gmail.com>
2021-11-16 12:35:49 +0100

    Refactoring by moving components into folders

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 
 import Layout from "../Layout"
-import CommentCard from "../comments/CommentCard"
+import CommentCard from "../commentscard/CommentCard"
 
 const Dashboard = () => {
   const [userComments, setUserComments] = useState([])
