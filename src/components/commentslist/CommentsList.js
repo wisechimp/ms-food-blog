4813560bce439d5322fa9adaf0b26193756263fb
shicks1080 <stuart.hicks@gmail.com>
2022-02-25 09:39:42 +0100

    Adding a catch if the comments can't be fetched

--- src/components/commentslist/CommentsList.js
+++ src/components/commentslist/CommentsList.js
@@ -1,38 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 
-import CommentCard from '../commentscard/CommentCard'
+import CommentCard from "../commentscard/CommentCard"
 
 const CommentsList = () => {
   const [postComments, setPostComments] = useState([])
-  const noComment = "There are no comments yet on this post. Signup or Login to join the conversation."
-  
+  const noComment =
+    "There are no comments yet on this post. Signup or Login to join the conversation."
+
   useEffect(() => {
-    console.log('Using effect')
-    fetch("http://localhost:3000/comments/" + window.location.pathname.replace("/posts/", ""))
-      .then(response => response.json())
-      .then(resultData => {setPostComments(resultData)})
-      .then(console.log(postComments))
-  }, [])
-  
-  return (
-    !postComments.length ? (
-      <p>
-        {noComment}
-      </p>
-    ) : (
-      <div>
-        {postComments.map(comment => {
-          return (
-            <CommentCard
-              key={comment.id}
-              commentUserName={comment.username}
-              commentDate={comment.date}
-              commentText={comment.text}
-            />
-          )
-        })}
-      </div>
+    console.log("Using effect")
+    fetch(
+      "http://localhost:3000/comments/" +
+        window.location.pathname.replace("/posts/", "")
     )
+      .then(response => response.json())
+      .then(resultData => {
+        setPostComments(resultData)
+      })
+      .then(console.log(postComments))
+      .catch(err => console.log(err))
+  }, [])
+
+  return !postComments.length ? (
+    <p>{noComment}</p>
+  ) : (
+    <div>
+      {postComments.map(comment => {
+        return (
+          <CommentCard
+            key={comment.id}
+            commentUserName={comment.username}
+            commentDate={comment.date}
+            commentText={comment.text}
+          />
+        )
+      })}
+    </div>
   )
 }
 
