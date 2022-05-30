eac72fae434511106b77b955f0e4e1c83fa71977
shicks1080 <stuart.hicks@gmail.com>
2022-05-30 15:05:05 +0200

    Adding a Word Cloud displaying all the tags

--- src/components/tagsWordcloud/TagsWordcloud.js
+++ src/components/tagsWordcloud/TagsWordcloud.js
@@ -1,8 +1,27 @@
 import React from "react"
-import ReactWordcloud from "react-wordcloud"
+import WordCloud from "react-d3-cloud"
+import { navigate } from "gatsby"
 
-const TagsWordcloud = ({ words }) => {
-  return <ReactWordcloud words={words} />
+import * as styles from "./tagswordcloud.module.css"
+
+const TagsWordcloud = ({ data, height, width }) => {
+  console.log(data)
+
+  return (
+    <div>
+      <div className={styles.tagsWordcloud}>
+        <WordCloud
+          data={data}
+          height={height}
+          width={width}
+          fontSize={word => word.value * 20}
+          onWordClick={(event, word) => {
+            navigate(`/tags/${word.text}/`)
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default TagsWordcloud
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { graphql } from "gatsby"
+import { getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/Layout"
 import BlogPostCard from "../components/blogpostcard/BlogPostCard"
-import { getImage } from "gatsby-plugin-image"
+import TagsWordcloud from "../components/tagsWordCloud/TagsWordCloud"
 
 const Posts = ({ data }) => {
+  const [wordCloudWidth, setWordCloudWidth] = useState(700)
+
+  const extractTags = () => {
+    const extractedTags = []
+    data.allMdx.edges.forEach(({ node }) => {
+      node.frontmatter.tags.forEach(tag => {
+        extractedTags.push(tag)
+      })
+    })
+    return extractedTags
+  }
+
+  const countTagOccurence = extractTags().reduce((count, tag) => {
+    count[tag] = (count[tag] || 0) + 1
+    return count
+  }, {})
+
+  const generateWordCloudObject = () => {
+    const wordCloudObject = Object.keys(countTagOccurence).map(tag => {
+      return {
+        text: tag,
+        value: countTagOccurence[tag],
+      }
+    })
+    return wordCloudObject
+  }
+
+  useEffect(() => {
+    const windowWidth = window.innerWidth
+    console.log(windowWidth)
+    if (windowWidth < 700) {
+      setWordCloudWidth(windowWidth)
+    }
+  }, [])
+
   return (
     <Layout title="Posts">
+      <TagsWordcloud
+        data={generateWordCloudObject()}
+        height={300}
+        width={wordCloudWidth}
+      />
       {data.allMdx.edges.map(({ node }) => {
         const imageData = getImage(node.frontmatter.imageSrc)
         return (
@@ -38,6 +79,7 @@ export const postsQuery = graphql`
             date(formatString: "Do MMMM YYYY")
             title
             author
+            tags
             imageSrc {
               childImageSharp {
                 gatsbyImageData(
