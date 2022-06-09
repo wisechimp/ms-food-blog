dbcd057e14b23aca1a18203238c68f8245346d74
shicks1080 <stuart.hicks@gmail.com>
2022-06-09 12:23:53 +0200

    feat: Add the site's layout to the tag page template

--- src/templates/tagpostslist.js
+++ src/templates/tagpostslist.js
@@ -1,47 +1,48 @@
-import React from 'react'
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import { Link, graphql } from 'gatsby'
+import Layout from "../components/Layout"
 
 const TagsPostTemplate = ({ pageContext, data }) => {
-	const { tag } = pageContext
-	const { edges, totalCount } = data.allMdx
-	const tagHeader = `${totalCount} post${
-		totalCount === 1 ? "" : "s"
-	} tagged with "${tag}"`
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMdx
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } tagged with "${tag}"`
   return (
-    <div>
-      <h1>{tagHeader}</h1>
+    <Layout title={tagHeader}>
       <ul>
-				{edges.map(({ node }) => {
-					const { title, slug } = node.frontmatter
-					return (
-						<li key={slug}>
-							<Link to={slug}>{title}</Link>
-						</li>
-					)
-				})}
-			</ul>
-    </div>
+        {edges.map(({ node }) => {
+          const { title, slug } = node.frontmatter
+          return (
+            <li key={slug}>
+              <Link to={slug}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
   )
 }
 
 export default TagsPostTemplate
 
 export const tagsQuery = graphql`
-	query($tag: String) {
-		allMdx(
-			limit: 2000
-			sort: { fields: [frontmatter___date], order: DESC }
-			filter: { frontmatter: { tags: { in: [$tag] } } }
-		) {
-			totalCount
-			edges {
-				node{
-					frontmatter {
-						title
-						slug
-					}
-				}
-			}
-		}
-	}`
+  query($tag: String) {
+    allMdx(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`
