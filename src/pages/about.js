276955c0943827ee936fcd4e2fe671f4fc3b7bd2
shicks1080 <stuart.hicks@gmail.com>
2022-09-24 10:32:07 +0200

    content: Adding Plum & Lavender posts
    Adding the "Plum Compot" and "Cooking with Lavender" posts.
    Updating the "About Me" page content.

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,8 +23,36 @@ const About = () => (
         sustainable in the long run.
       </p>
       <p>
+        It also gives me great satisfaction to know that this dietary balance is
+        more sustainable for our planet too, and as an extra bonus is typically
+        more economical. Having had this balance in diet at home for the boys
+        from the beginning also means it is quite natural for them to eat this
+        way. This has left them more open-minded about foods also in school and
+        day-care. I cannot force their choice of foods in school nor later on in
+        life, but at least I have given them a good grounding.
+      </p>
+      <p>
+        The shift from a diet with plenty of meat to one with a majority of veg
+        (not that they were ever absent) happened gradually for me, with a lot
+        of experimentation. Changing everything at once is too dramatic and too
+        hard work to last in the long run. In other words, I encourage you to
+        also experiment and to make any shifts to a more healthy and sustainable
+        diet a gradual one.
+      </p>
+      <p>
         Here I will blog about my recipes and my ideas for how to approach food
-        and time for cooking when also dealing with a busy schedule
+        and time for cooking when also dealing with a busy schedule. I will try
+        to show you how you can also make your cooking more sustainable - for
+        yourself and for the planet you live on, while being mindful of the time
+        you have available (or not!) After all, while cooking might be fun, it
+        has to fit with the time YOU have available.
+      </p>
+      <p>
+        In line with my idea of being sustainable, I will also be mindful of not
+        wasting food, just because I am trying to also write about it or take
+        photographs of it. The food that goes into my creations will be eaten,
+        and I will try to not waste additional resources just for the sake of my
+        blog.
       </p>
     </div>
   </Layout>
