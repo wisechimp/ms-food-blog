1318cdf9ba33961dcbdedb2a85d7825b2157e4ca
shicks1080 <stuart.hicks@gmail.com>
2023-02-03 14:28:47 +0100

    fix: Updating the schemas to account for an image object
    So I created the Main Image object to include altText with the image. This deployed to Sanity Studio fine, but the deploy to graphql was unacceptable. It seems that inline objects aren't allowed. Therefore as advised I extracted the Main Image as a separate schema creating a new type.

--- ms-food-blog-backend/schemas/index.ts
+++ ms-food-blog-backend/schemas/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import tag from './tag'
 import post from './post'
 import author from './author'
+import mainImageObject from './mainImageObject'
 
-export const schemaTypes = [post, author, tag, blockContent]
+export const schemaTypes = [post, author, tag, blockContent, mainImageObject]
---
+++ ms-food-blog-backend/schemas/mainImageObject.ts
@@ -0,0 +1,23 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'mainImageObject',
+  title: 'Main Image Object',
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'mainImage',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'altText',
+      title: 'Alternative text',
+      description: 'This is required for people who rely on screen readers and SEO.',
+      type: 'string',
+    }),
+  ],
+})
