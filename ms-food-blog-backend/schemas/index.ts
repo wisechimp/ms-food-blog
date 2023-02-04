f8c9cbb4231c4be10e7ebdcc14f86fc1cf5e387b
shicks1080 <stuart.hicks@gmail.com>
2023-02-04 16:57:07 +0100

    housekeeping: Moving the custom schema to it's own folder
    Trying to make the top level schema folder a little tidy, at least that's the plan!

--- ms-food-blog-backend/schemas/index.ts
+++ ms-food-blog-backend/schemas/index.ts
@@ -2,6 +2,6 @@ import blockContent from './blockContent'
 import tag from './tag'
 import post from './post'
 import author from './author'
-import mainImageObject from './mainImageObject'
+import mainImageObject from './customSchemas/mainImageObject'
 
 export const schemaTypes = [post, author, tag, blockContent, mainImageObject]
