02db2820afa2e6af6c5c69be49ad9ffbb1e24c31
shicks1080 <stuart.hicks@gmail.com>
2023-02-04 21:12:16 +0100

    feat: Adding icons to Sanity schemas
    Added react-icons.
    Requires renaming of files from ts to tsx.

--- ms-food-blog-backend/schemas/tag.tsx
+++ ms-food-blog-backend/schemas/tag.tsx
@@ -1,9 +1,11 @@
 import {defineField, defineType} from 'sanity'
+import {GrTag} from 'react-icons/gr'
 
 export default defineType({
   name: 'tag',
   title: 'Tag',
   type: 'document',
+  icon: GrTag,
   fields: [
     defineField({
       name: 'title',
