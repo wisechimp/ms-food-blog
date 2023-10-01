726000af31a26e9eef50f092c2c28fb59bdd3cd2
shicks1080 <stuart.hicks@gmail.com>
2023-10-01 12:32:10 +0200

    feat: Adding Contact Form

--- app/components/contactform/contactform.js
+++ app/components/contactform/contactform.js
@@ -6,10 +6,10 @@ import Recaptcha from 'react-google-recaptcha'
 
 import * as contactformStyles from './contactform.module.css'
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
-if (typeof RECAPTCHA_KEY === 'undefined') {
+const WHAT_THE_WHAT = "6LefSsYcAAAAAAILshfN7XPOS-D9B9S3TKNILJ5n"
+if (typeof WHAT_THE_WHAT === 'undefined') {
     throw new Error(`
-    Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
+    Env var WHAT_THE_WHAT is undefined! 
     You probably forget to set it in your Netlify build environment variables. 
     Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
     Note this demo is specifically for Recaptcha v2
@@ -118,7 +118,7 @@ const ContactForm = () => {
                 onChange={handleChange}
               />
             </p>
-            <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
+            <Recaptcha ref={recaptchaRef} sitekey={WHAT_THE_WHAT} />
             <button className={contactformStyles.formButton} type="submit">
               Send Message
             </button>
