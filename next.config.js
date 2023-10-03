fcac121376ed7e89a54cf544d63dff50bb85bb5c
shicks1080 <stuart.hicks@gmail.com>
2023-10-03 10:30:52 +0200

    fix: Downgrading Next version
    Netlify doesn't support 13.5 according to their support techs and that "could" be why studio isn't running properly (although I get the impression they don't really know, we'll see if this fix works...).

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    appDir: true,
     serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 }
 
